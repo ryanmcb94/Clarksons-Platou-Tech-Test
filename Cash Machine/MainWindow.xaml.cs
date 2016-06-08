1c1557fde6e7091690fa508e3d44d365e0ccf3b9
Ryan McBroom <ryan.mcbroom@icloud.com>
2016-06-08 19:24:19 +0100

    Cash Machine

---
+++ Cash Machine/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cash_Machine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Define Variables
+        private Program pgm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Setup system.
+            this.pgm = new Program();
+            this.pgm.setup();
+            this.displayBalance();
+        }
+
+
+        /// <summary>
+        /// Alg 1 least items
+        /// </summary>
+        private void btnAlg1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.displayWithdraw(this.pgm.Algorithm1(Convert.ToDecimal(txtInput.Text)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Input is empty or not a valid input. Correct Format XXX.XX","Format Error");
+            }
+            
+        }
+
+        /// <summary>
+        /// Alg 2 - Max £20
+        /// </summary>
+        private void btnAlg2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.displayWithdraw(this.pgm.Algorithm2(Convert.ToDecimal(txtInput.Text)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Input is empty or not a valid input. Correct Format XXX.XX", "Format Error");
+            }
+        }
+
+
+        /// <summary>
+        /// Displays the amount being withdrawn from the system.
+        /// </summary>
+        /// <param name="withdraw"></param>
+        private void displayWithdraw(Dictionary<decimal, int> withdraw)
+        {
+            
+            string strWithdraw = "Withdraw: \n";
+            foreach (decimal key in withdraw.Keys)
+            {
+                if (withdraw[key] > 0)
+                {
+                    if (key > 1) //if pound
+                    {
+                        strWithdraw += string.Format("£{0} x {1} \n",key, withdraw[key]);
+                    }
+                    else
+                    {
+                        string strKey = String.Format("{0:0.00}", key);
+                        strWithdraw += string.Format("{0}p x {1} \n", strKey, withdraw[key]);
+                    }
+                }
+            }
+            txtReturned.Text = strWithdraw;
+        }
+
+        /// <summary>
+        /// Displays balance left in the system.
+        /// </summary>
+        private void displayBalance()
+        {
+            string balance="Current Balance: \n";
+            foreach(decimal key in this.pgm.balance.Keys)
+            {
+                if(this.pgm.balance[key] >0)
+                {
+                    if(key >1) //if pound
+                    {
+                        balance += string.Format("£{0} x {1} \n", key, this.pgm.balance[key]);
+                    }
+                    else
+                    {
+                        balance += string.Format("{0}p x {1} \n", key, this.pgm.balance[key]);
+                    }
+                }
+            }
+            txtBalance.Text = balance;
+        }
+
+        /// <summary>
+        /// Reset Balance.
+        /// </summary>
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            this.pgm.setup();
+        }
+    }
+}
---
+++ Cash Machine/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Cash_Machine
+{
+    class Program
+    {
+        //Define Variables
+        public Dictionary<decimal, int> balance { get; set; } //Stores inital states with key being item value and value being the number of each item left
+
+        /// <summary>
+        /// Add Values to the system.
+        /// </summary>
+        public void setup()
+        {
+            this.balance = new Dictionary<decimal, int>();
+            balance.Add(50, 50);
+            balance.Add(20, 50);
+            balance.Add(10, 50);
+            balance.Add(5, 50);
+            balance.Add(2, 100);
+            balance.Add(1, 100);
+            balance.Add((decimal)0.50, 100);
+            balance.Add((decimal)0.20, 100);
+            balance.Add((decimal)0.10, 100);
+            balance.Add((decimal)0.02, 100);
+            balance.Add((decimal)0.01, 100);
+        }
+
+        /// <summary>
+        /// Get Total Amount Left
+        /// </summary>
+        /// <returns>Double with value</returns>
+        public decimal getBalance()
+        {
+            decimal value = 0;
+            foreach (decimal key in balance.Keys)
+            {
+                value += key * balance[key];
+            }
+            return value;
+        }
+
+        /// <summary>
+        /// Alg1 - Least amount of items
+        /// </summary>
+        /// <param name="amount">Amount to be withdrawn</param>
+        public Dictionary<decimal, int> Algorithm1(decimal amount)
+        {
+            Dictionary<decimal, int> toGive = new Dictionary<decimal, int>();
+            //Check if balance > amount
+            if (this.getBalance() < amount)
+                MessageBox.Show("Not enough funds in cash machine");
+            else
+            {
+                foreach (decimal key in this.balance.Keys)
+                {
+                    if (amount > 0)
+                    {
+                        int num = Convert.ToInt32(Math.Floor(amount / key)); // Find how many of the current item can be applied to the amount
+                        if (this.balance[key] > num && num > 0) // of the is more than the number of items that can be applied to the amount
+                        {
+                            toGive.Add(key, num);
+                            amount -= key * num;
+                        }
+                        else if (this.balance[key] > 0 && num > 0)
+                        {
+                            toGive.Add(key, this.balance[key]);
+                            amount -= key * this.balance[key];
+                        }
+                    }
+                    else
+                    {
+
+                        break;
+                    }
+
+                }
+            }
+
+            foreach (decimal key in toGive.Keys) //Update Balance.
+            {
+                this.balance[key] = this.balance[key] - toGive[key];
+            }
+            return toGive;
+
+        }
+
+        /// <summary>
+        /// Alg2 - Max £20
+        /// </summary>
+        /// <param name="amount">Amount to be withdrawn</param>
+        public Dictionary<decimal, int> Algorithm2(decimal amount)
+        {
+            Dictionary<decimal, int> toGive = new Dictionary<decimal, int>();
+            //Check if balance > amount
+            if (this.getBalance() < amount)
+                MessageBox.Show("Not enough funds in cash machine");
+            else 
+            {
+                if (amount > 0) //£20 Notes
+                {
+                    int num = Convert.ToInt32(Math.Floor(amount / 20));
+                    if (this.balance[20] > num && num > 0)
+                    {
+                        toGive.Add(20, num);
+                        amount -= 20 * num;
+                    }
+                }
+                foreach (decimal key in this.balance.Keys) //All other values.
+                {
+                    if (amount > 0)
+                    {
+                        if (key != 20)
+                        {
+                            int num = Convert.ToInt32(Math.Floor(amount / key)); // Find how many of the current item can be applied to the amount
+                            if (this.balance[key] > num && num > 0) // of the is more than the number of items that can be applied to the amount
+                            {
+                                toGive.Add(key, num);
+                                amount -= key * num;
+                            }
+                            else if (this.balance[key] > 0 && num > 0)
+                            {
+                                toGive.Add(key, this.balance[key]);
+                                amount -= key * this.balance[key];
+                            }
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            foreach (decimal key in toGive.Keys) //Update Balance.
+            {
+                this.balance[key] = this.balance[key] - toGive[key];
+            }
+            return toGive;
+        }
+    }
+}
